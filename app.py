da29ad6848859ee5048a80f017d247a902869e50
Vikctar <vikc.steve@gmail.com>
2018-12-30 00:46:54 +0300

    creating blog posts

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
 from database import Database
+from models.blog import Blog
 from models.post import Post
 
 Database.initialize()
 
 post = Post.from_mongo('b1c95b9aeb364627ad87168cde32b31f')
-print(post)
+# print(post)
 
 posts = Post.from_blog('123')
-for p in posts:
-    print(p)
+
+blog = Blog(author='Vikc',
+            title='New New',
+            description='New description')
+
+blog.new_post()
+
+blog.save_to_mongo()
+
+from_database = Blog.from_mongo(blog.id)
+
+print(blog.get_posts())
